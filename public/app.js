982580488c10188428ba0ef92a480c5b0ec13d9c
Michael Snaddon <msnaddon0991@gmail.com>
2016-10-02 20:27:37 +0100

    sorting by distance adds that attirute to city for sorting

--- public/app.js
+++ public/app.js
@@ -1,4 +1,6 @@
 var map;
+var currentLatLon;
+
 
 var getScanArea = function(latlon, range){
   // generate array of left,top,right,bottom for scanning box
@@ -53,13 +55,38 @@ var getNearestSunshine = function(){
     return city.weather[0].id === 800;
   })
   publishWeatherReport(sunnyCities);
+  // do something will not quite sunny weather?
+}
+
+
+var distanceCalc = function(lat, lon){
+  var latDif = Math.abs(currentLatLon.lat - lat);
+  var lonDif = Math.abs(currentLatLon.lon - lon);
+  var distance = Math.sqrt(Math.pow(latDif,2) + Math.pow(lonDif,2))
+  console.log("distance calculated:", distance)
+  return distance
+}
+
+var sortByDistance = function(citiesArray){
+  citiesArray.forEach(function(item){
+    item.distance=distanceCalc(item.coord.lat, item.coord.lon);
+  })
+  console.log(citiesArray[0], citiesArray[1]);
+  citiesArray.sort(function(itemA, itemB){
+    return itemA.distance-itemB.distance;
+  })
+  return citiesArray;
 }
 
 // present information on app page
 var publishWeatherReport = function(citiesArray){
+
+  citiesArray = sortByDistance(citiesArray)
+
+
+
   // get weather report element
   var reportList = document.querySelector("#weather-report");
-
   //check for sunny locations
   if (citiesArray.length !== 0){
     // add cities to report
@@ -72,9 +99,10 @@ var publishWeatherReport = function(citiesArray){
   }
 }
 
-
+// needs some work to make the appearance better, maybe even a flickr api and animations?
 var addWeatherReport = function(parent, cityObject){
-  console.log(cityObject.weather)
+  // console.log(cityObject.weather[0])
+  console.log("weather report added")
   var weatherBlock = document.createElement('p')
   weatherBlock.innerText = cityObject.name + " weather is " +cityObject.weather[0].description
   parent.appendChild(weatherBlock)
@@ -88,39 +116,35 @@ var addMapMarker = function(latlon, label){
     map: map,
     label: label
   })
-  console.log(latLng)
+  // console.log(latLng)
 }
-
 
 
 
 var onPageLoad = function(){
   // make map
-   map = new google.maps.Map(document.getElementById('gmap'), {
-     // center: {lat: 0, lng: 0},
+  map = new google.maps.Map(document.getElementById('gmap'), {
      zoom: 7
    });
 
   //get users current position
   if ("geolocation" in navigator){
-    // update report on page for current location.
     navigator.geolocation.getCurrentPosition(function(position) {
-      var currentLatLon = {
+      //assign global position
+      currentLatLon = {
         lat: position.coords.latitude, 
         lon: position.coords.longitude
       }; 
-      
-      // pittsburgh america test
-      // currentLatLon={lat:40, lon:-80}
 
+
+      // update page with new location
       map.setCenter({lat: currentLatLon.lat, lng: currentLatLon.lon})
 
       addMapMarker(currentLatLon, "1")
       generateNewReport(currentLatLon, 1.5);
     });
   }
-
-  }
+}
 
 window.onload = onPageLoad;
 
@@ -130,6 +154,11 @@ window.onload = onPageLoad;
 
 
 // DEPRECATED CODE
+
+
+// pittsburgh america test
+// currentLatLon={lat:40, lon:-80}
+
 
 // var generateNewReport = function(latlon, cityCount){
 //   console.log("generateNewReport invoked")
